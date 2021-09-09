713c26100d6d044511c548e3f83a39efc51589df
BigGravy <90368946+BigGravy@users.noreply.github.com>
2021-09-08 23:53:25 -0400

    Add unreal project

---
+++ Source/TeacherGareth/TeacherGareth.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TeacherGareth.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeacherGareth, "TeacherGareth" );
+ 
---
+++ Source/TeacherGareth/TeacherGarethGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TeacherGarethGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ATeacherGarethGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ATeacherGarethGameMode();
+};
+
+
+
